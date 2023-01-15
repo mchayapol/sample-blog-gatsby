15bdd4e760c20e118e090f266d8d268e2cde6709
Chayapol Moemeng <mchayapol@gmail.com>
2023-01-15 22:21:52 +0700

    Initial commit from gatsby

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const BlogPage = () => {
+  return <main>
+    <h1>Blogs</h1>
+  </main>;
+};
+
+export default BlogPage;
+
+export const Head = () => <title>Blogs</title>;
